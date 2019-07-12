72c78293b03715ef280e6baaa68cd051ff2a84d2
Veselov Pavel <pvsaintpe@icloud.com>
2019-07-12 22:21:01 +0300

    fix expression namespace

--- src/Compilers/ChangeColumnCompiler.php
+++ src/Compilers/ChangeColumnCompiler.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Umbrellio\Postgres\Compilers;
 
 use Doctrine\DBAL\Schema\Expressions\Expression;
+use Illuminate\Database\Query\Expression as BaseExpression;
 use Illuminate\Database\Schema\Grammars\ChangeColumn;
 
 class ChangeColumnCompiler extends ChangeColumn
 {
     protected static function mapFluentValueToDoctrine($option, $value)
     {
-        if ($option === 'default' && $value instanceof Expression) {
+        if ($option === 'default' && $value instanceof BaseExpression) {
             return new Expression($value);
         }
         return parent::mapFluentValueToDoctrine($option, $value);

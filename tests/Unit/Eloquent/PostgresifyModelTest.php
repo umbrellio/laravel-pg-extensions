7561738c6fad4d2588b8c7b45d5f2241e634d10c
Veselov Pavel <pvsaintpe@icloud.com>
2019-07-12 22:08:07 +0300

    fix coverage

--- tests/Unit/Eloquent/PostgresifyModelTest.php
+++ tests/Unit/Eloquent/PostgresifyModelTest.php
@@ -15,29 +15,28 @@ class PostgresifyModelTest extends TestCase
      * @dataProvider provideIntervals
      * @test
      */
-    public function transform(string $interval, Carbon $periodStart, Carbon $periodEnd): void
+    public function transform(string $name, string $interval, Carbon $periodStart, Carbon $periodEnd): void
     {
-        $model = new class() extends PostgresifyModel {
+        $model = new class(compact('interval', 'name')) extends PostgresifyModel {
             protected $postgresifyTypes = [
                 'interval' => [
                     'type' => 'dateRange',
                 ],
             ];
 
-            protected $fillable = ['interval'];
+            protected $fillable = ['interval', 'name'];
         };
 
-        $model->fill(compact('interval'));
-
         $this->assertSame(implode(',', [$periodStart, $periodEnd]), (string) $model->interval);
+        $this->assertSame($name, $model->name);
         $this->assertIsArray($model->interval->toArray());
         $this->assertCount(2, (array) $model->interval);
     }
 
     public function provideIntervals(): Generator
     {
-        yield ['2019-01-01,2019-01-31', Carbon::create(2019)->firstOfMonth(), Carbon::create(2019)->lastOfMonth()];
-        yield ['2019-01-01,2019-03-31', Carbon::create(2019)->firstOfQuarter(), Carbon::create(2019)->lastOfQuarter()];
-        yield ['2019-01-01,2019-12-31', Carbon::create(2019)->firstOfYear(), Carbon::create(2019)->lastOfYear()];
+        yield ['name', '2019-01-01,2019-01-31', Carbon::create(2019)->firstOfMonth(), Carbon::create(2019)->lastOfMonth()];
+        yield ['name', '2019-01-01,2019-03-31', Carbon::create(2019)->firstOfQuarter(), Carbon::create(2019)->lastOfQuarter()];
+        yield ['name', '2019-01-01,2019-12-31', Carbon::create(2019)->firstOfYear(), Carbon::create(2019)->lastOfYear()];
     }
 }

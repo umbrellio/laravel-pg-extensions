02e61cbfda14f93e17175aeeb1a9793d5c06531e
Veselov Pavel <pvsaintpe@icloud.com>
2019-07-23 21:29:52 +0300

    testing travis

--- tests/travis/install-postgres-11.sh
+++ tests/travis/install-postgres-11.sh
@@ -10,5 +10,3 @@ sudo docker run -d --name postgres11 -p 5432:5432 postgres:11.1
 sudo docker exec -i postgres11 bash <<< 'until pg_isready -U postgres > /dev/null 2>&1 ; do sleep 1; done'
 
 echo "Postgres 11 ready"
-
-pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start

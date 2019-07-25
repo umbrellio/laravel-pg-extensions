3fc5f4d0d243884cfafedb467a1190b8bcd9fb35
Veselov Pavel <pvsaintpe@icloud.com>
2019-07-26 02:18:56 +0300

    some fixes travis (add dummy tests)

--- tests/Unit/Schema/Subscribers/ChangeColumnSubscriberTest.php
+++ tests/Unit/Schema/Subscribers/ChangeColumnSubscriberTest.php
@@ -2,64 +2,247 @@
 
 declare(strict_types=1);
 
-namespace Umbrellio\Postgres\Tests\Unit\Extensions;
+namespace Umbrellio\Postgres\Tests\Unit\Subscribers;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\ServiceProvider;
-use Umbrellio\Postgres\Extensions\AbstractComponent;
-use Umbrellio\Postgres\Extensions\AbstractExtension;
-use Umbrellio\Postgres\Extensions\Exceptions\MacroableMissedException;
-use Umbrellio\Postgres\Extensions\Exceptions\MixinInvalidException;
-use Umbrellio\Postgres\Schema\Blueprint;
+use Doctrine\DBAL\Event\SchemaAlterTableChangeColumnEventArgs;
+use Doctrine\DBAL\Events;
+use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\ColumnDiff;
+use Doctrine\DBAL\Schema\TableDiff;
+use Doctrine\DBAL\Types\Type;
+use Illuminate\Support\Collection;
+use Umbrellio\Postgres\Schema\Subscribers\SchemaAlterTableChangeColumnSubscriber;
 use Umbrellio\Postgres\Tests\TestCase;
 
-class AbstractExtensionTest extends TestCase
+/**
+ * @property SchemaAlterTableChangeColumnSubscriber $subscriber
+ * @property PostgreSqlPlatform $platform
+ * @property TableDiff $tableDiff
+ * @property ColumnDiff $columnDiff
+ * @property SchemaAlterTableChangeColumnEventArgs $eventArgs
+ * @property Column $column
+ */
+class ChangeColumnSubscriberTest extends TestCase
 {
-    /** @test */
-    public function registerInvalidExtension(): void
+    private const TABLE = 'some_table';
+    private const COLUMN = 'some_field';
+
+    private $subscriber;
+    private $platform;
+    private $tableDiff;
+    private $columnDiff;
+    private $eventArgs;
+    private $column;
+
+    protected function setUp(): void
     {
-        $abstractExtension = new class() extends AbstractExtension {
-            public static function getName(): string
-            {
-                return 'extension';
-            }
+        parent::setUp();
 
-            public static function getMixins(): array
-            {
-                return [
-                    Blueprint::class => new class() extends Model {
-                    },
-                ];
-            }
-        };
+        $this->subscriber = new SchemaAlterTableChangeColumnSubscriber();
+        $this->platform = new PostgreSqlPlatform();
 
-        $this->expectException(MixinInvalidException::class);
+        $this->column = new Column(
+            static::COLUMN,
+            Type::getType('integer'),
+            [
+                // column options
+            ]
+        );
 
-        /** @var AbstractExtension $abstractExtension */
-        $abstractExtension::register();
+        $this->tableDiff = new TableDiff(
+            static::TABLE,
+            [],
+            [
+                // changed columns
+            ],
+            [],
+            [],
+            [],
+            [],
+            null
+        );
+
+        $this->columnDiff = new ColumnDiff(
+            static::COLUMN,
+            $this->column,
+            [
+                // changed properties
+            ],
+            $this->column
+        );
+
+        $this->eventArgs = new SchemaAlterTableChangeColumnEventArgs(
+            $this->columnDiff,
+            $this->tableDiff,
+            $this->platform
+        );
     }
 
     /** @test */
-    public function registerWithInvalidMixin(): void
+    public function getSubscriberEvents(): void
     {
-        $abstractExtension = new class() extends AbstractExtension {
-            public static function getName(): string
-            {
-                return 'extension';
-            }
+        $this->assertSame([Events::onSchemaAlterTableChangeColumn], $this->subscriber->getSubscribedEvents());
+    }
 
-            public static function getMixins(): array
-            {
-                return [
-                    ServiceProvider::class => new class() extends AbstractComponent {
-                    },
-                ];
-            }
-        };
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function onSchemaAlterTableChangeColumn(): void
+    {
+        $this->subscriber->onSchemaAlterTableChangeColumn($this->eventArgs);
+        $this->markTestIncomplete();
+    }
 
-        $this->expectException(MacroableMissedException::class);
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function getAlterTableChangeColumnSQL(): void
+    {
+        $this->subscriber->onSchemaAlterTableChangeColumn($this->eventArgs);
+        $this->markTestIncomplete();
+    }
 
-        /** @var AbstractExtension $abstractExtension */
-        $abstractExtension::register();
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function compileAlterColumnComment(): void
+    {
+        $this->subscriber->compileAlterColumnComment(
+            $this->platform,
+            $this->columnDiff,
+            $this->column,
+            static::COLUMN,
+            new Collection()
+        );
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function compileAlterColumnNull(): void
+    {
+        $this->subscriber->compileAlterColumnNull(
+            $this->columnDiff,
+            $this->column,
+            static::COLUMN,
+            static::COLUMN,
+            new Collection()
+        );
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function compileAlterColumnDefault(): void
+    {
+        $this->subscriber->compileAlterColumnDefault(
+            $this->platform,
+            $this->columnDiff,
+            $this->column,
+            static::COLUMN,
+            static::COLUMN,
+            new Collection()
+        );
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function compileAlterColumnSequence(): void
+    {
+        $this->subscriber->compileAlterColumnSequence(
+            $this->platform,
+            $this->columnDiff,
+            $this->tableDiff,
+            $this->column,
+            static::COLUMN,
+            static::COLUMN,
+            new Collection()
+        );
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function compileAlterColumnType(): void
+    {
+        $this->subscriber->compileAlterColumnType(
+            $this->platform,
+            $this->columnDiff,
+            $this->column,
+            static::COLUMN,
+            static::COLUMN,
+            new Collection()
+        );
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function getDefaultValueDeclarationSQL(): void
+    {
+        $this->subscriber->getDefaultValueDeclarationSQL($this->platform, $this->column);
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function typeChangeBreaksDefaultValue(): void
+    {
+        $this->subscriber->typeChangeBreaksDefaultValue($this->columnDiff);
+        $this->markTestIncomplete();
+    }
+
+    /** @test */
+    public function isNumericType(): void
+    {
+        $this->assertTrue($this->subscriber->isNumericType(Type::getType('integer')));
+        $this->assertFalse($this->subscriber->isNumericType(Type::getType('string')));
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function quoteName(): void
+    {
+        $this->subscriber->quoteName($this->platform, $this->tableDiff);
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function getOldColumnComment(): void
+    {
+        $this->subscriber->getOldColumnComment($this->columnDiff);
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @test
+     * @todo Допилить тесты
+     */
+    public function getColumnComment(): void
+    {
+        $this->subscriber->getColumnComment($this->column);
+        $this->markTestIncomplete();
     }
 }

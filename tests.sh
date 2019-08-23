b18a539ec18bac7ea6676287c5ecca5c8d3be31c
Veselov Pavel <pvsaintpe@icloud.com>
2019-08-23 17:38:30 +0300

    fix exclude groups

--- tests.sh
+++ tests.sh
@@ -2,4 +2,8 @@
 
 psql postgres -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'testing'" | grep -q 1 || psql postgres -U postgres -c "CREATE DATABASE testing"
 composer lint
-php -d pcov.directory='.' vendor/bin/phpunit --coverage-html build
+if [ "x$EXCLUDE_GROUP" != "x" ]; then
+    php -d pcov.directory='.' vendor/bin/phpunit --exclude-group $EXCLUDE_GROUP --coverage-html build
+else
+    php -d pcov.directory='.' vendor/bin/phpunit --coverage-html build
+fi

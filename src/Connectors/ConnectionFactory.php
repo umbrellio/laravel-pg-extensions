ac45ba7e408c44608cded615a780482e63327172
Korben Dallas <pvsaintpe@icloud.com>
2020-12-10 21:12:45 +0300

    fixed #39

--- src/Connectors/ConnectionFactory.php
+++ src/Connectors/ConnectionFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Umbrellio\Postgres\Connectors;
 
+use Illuminate\Database\Connection;
 use Illuminate\Database\Connectors\ConnectionFactory as ConnectionFactoryBase;
 use Umbrellio\Postgres\PostgresConnection;
 
@@ -11,6 +12,14 @@ class ConnectionFactory extends ConnectionFactoryBase
 {
     protected function createConnection($driver, $connection, $database, $prefix = '', array $config = [])
     {
-        return new PostgresConnection($connection, $database, $prefix, $config);
+        if ($resolver = Connection::getResolver($driver)) {
+            return $resolver($connection, $database, $prefix, $config);
+        }
+
+        if ($driver === 'pgsql') {
+            return new PostgresConnection($connection, $database, $prefix, $config);
+        }
+
+        return parent::createConnection($driver, $connection, $database, $prefix, $config);
     }
 }

8ae59d7cd75b3919acc11df6a3ca5d6128608ac7
Korben Dallas <pvsaintpe@icloud.com>
2021-01-06 06:24:35 +0300

    CI improvements (#56)
    - Updated readme
    - Some fixes for local testing
    - Migrated to new format for phpunit config

--- tests.sh
+++ tests.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 psql postgres -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'testing'" | grep -q 1 || psql postgres -U postgres -c "CREATE DATABASE testing"
-composer lint
 sed -e "s/\${USERNAME}/postgres/" \
     -e "s/\${PASSWORD}//" \
     -e "s/\${DATABASE}/testing/" \
     -e "s/\${HOST}/127.0.0.1/" \
     phpunit.xml.dist > phpunit.xml
+composer update
+composer lint
 if [ "x$EXCLUDE_GROUP" != "x" ]; then
     php -d pcov.directory='.' vendor/bin/phpunit \
       --exclude-group $EXCLUDE_GROUP \

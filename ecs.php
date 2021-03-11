9a7d1679f6834e5d234798fbb59f032cae1fa953
Korben <pvsaintpe@icloud.com>
2021-03-11 23:50:19 +0300

    fix linnter

---
+++ ecs.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestAnnotationFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/vendor/umbrellio/code-style-php/umbrellio-cs.php');
+
+    $services = $containerConfigurator->services();
+
+    $services->set(PhpUnitTestAnnotationFixer::class)
+        ->call('configure', [[
+            'style' => 'annotation',
+        ]]);
+
+    $services->set(DeclareStrictTypesFixer::class);
+
+    $services->set(BinaryOperatorSpacesFixer::class)
+        ->call('configure', [[
+            'default' => 'single_space',
+        ]]);
+
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('cache_directory', '.ecs_cache');
+
+    $parameters->set('exclude_files', ['vendor/*', 'database/*']);
+};

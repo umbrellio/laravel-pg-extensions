c65b1f01ef5b628b4267240c907dc3ad09f3c43b
mpyw <ryosuke_i_628@yahoo.co.jp>
2023-12-21 20:06:52 +0900

    Fix compatibility with laravel/framework#49231 (#82)

--- src/Schema/Builder.php
+++ src/Schema/Builder.php
@@ -28,7 +28,7 @@ class Builder extends BasePostgresBuilder
         $this->build($blueprint);
     }
 
-    public function hasView(string $view): bool
+    public function hasView($view): bool
     {
         return count($this->connection->selectFromWriteConnection($this->grammar->compileViewExists(), [
             $this->connection->getConfig()['schema'],
